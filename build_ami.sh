c11d7fdbb5479bcff6255072fe21dd2750c30c1a
Nikunj Lad <lad.n@husky.neu.edu>
2019-07-26 19:28:02 -0400

    committing the curl build script

--- build_ami.sh
+++ build_ami.sh
@@ -3,6 +3,9 @@
 echo -n "Enter your github username: "
 read USER_NAME
 
+echo -n "Enter the branch name you want to build: "
+read BRANCH
+
 if [[ ${USER_NAME} == "" ]]; then
     echo "Please enter a your github  username!"
     exit 1
@@ -10,6 +13,13 @@ else
     echo "GitHub username : ${USER_NAME}"
 fi
 
+if [[ ${BRANCH} == "" ]]; then
+    echo "Please enter a valid branch name!"
+    exit 1
+else
+    echo "Building on branch : ${BRANCH}"
+fi
+
 curl -u ${CIRCLECI_TOKEN} \
      -d '{"build_parameters": {"[CIRCLE_JOB]":"build"}}' \
-     https://circleci.com/api/v1.1/project/github/${USER_NAME}/csye6225-su19-ami/tree/master
+     https://circleci.com/api/v1.1/project/github/${USER_NAME}/csye6225-su19-ami/tree/${BRANCH}
